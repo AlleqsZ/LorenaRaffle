af8c87e93cbf4613839f68f993252d7d522860a4
AlleqsZ <93009059+AlleqsZ@users.noreply.github.com>
2025-04-14 00:37:54 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,5 +15,8 @@ app.use(express.json())
 
 app.use('/api/auth', authRoutes)
 
+import prizeRoutes from './routes/prizes.js'
+app.use('/api/prizes', prizeRoutes)
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
