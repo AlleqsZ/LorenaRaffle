cae61676f57133e414bb4123316bf97ea2883867
AlleqsZ <93009059+AlleqsZ@users.noreply.github.com>
2025-04-14 00:39:16 +0100

    Update Ticket.js

--- backend/models/Ticket.js
+++ backend/models/Ticket.js
@@ -1,1 +1,28 @@
+import mongoose from 'mongoose'
 
+const ticketSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  prize: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Prize',
+    required: true
+  },
+  number: {
+    type: Number,
+    required: true
+  },
+  isWinner: {
+    type: Boolean,
+    default: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+export default mongoose.model('Ticket', ticketSchema)
