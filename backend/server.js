be342970f25f20c29b502b6d5fee1f019117d682
AlleqsZ <93009059+AlleqsZ@users.noreply.github.com>
2025-04-14 00:44:59 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import express from 'express'
+import dotenv import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import connectDB from './config/db.js'
 
 import authRoutes from './routes/auth.js'
+import prizeRoutes from './routes/prizes.js'
+import ticketRoutes from './routes/tickets.js' // <- AICI e pasul 3
 
 dotenv.config()
 connectDB()
@@ -14,9 +17,8 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/auth', authRoutes)
-
-import prizeRoutes from './routes/prizes.js'
 app.use('/api/prizes', prizeRoutes)
+app.use('/api/tickets', ticketRoutes) // <- AICI e linia necesară
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
