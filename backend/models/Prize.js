1c92f62aae437442a308b86ce0616b3cf568de86
AlleqsZ <93009059+AlleqsZ@users.noreply.github.com>
2025-04-14 00:35:34 +0100

    Update Prize.js

--- backend/models/Prize.js
+++ backend/models/Prize.js
@@ -1,1 +1,28 @@
+import mongoose from 'mongoose'
 
+const prizeSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  drawDate: {
+    type: Date,
+    required: true
+  },
+  winnerTicketNumber: {
+    type: Number,
+    default: null // pentru câștigător (dacă îl alegi tu)
+  }
+}, {
+  timestamps: true
+})
+
+export default mongoose.model('Prize', prizeSchema)
