079da1ce1c0fbdd8842705fbb8edd8c86dafdf2b
AlleqsZ <93009059+AlleqsZ@users.noreply.github.com>
2025-04-14 12:54:50 +0100

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,25 +1,21 @@
-import mongoose from 'mongoose'
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    unique: true,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  isAdmin: {
-    type: Boolean,
-    default: false
-  }
-}, {
-  timestamps: true
-})
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  isAdmin: { type: Boolean, default: false },
+});
 
-export default mongoose.model('User', userSchema)
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.methods.matchPassword = function(password) {
+  return bcrypt.compare(password, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
