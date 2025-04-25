dfb533a4e9e1f39950420defc7f9c435c5e24a23
AlleqsZ <93009059+AlleqsZ@users.noreply.github.com>
2025-04-25 16:43:58 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -22,3 +22,7 @@ app.use('/api/tickets', ticketRoutes) // <- AICI e linia necesară
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+import raffleRoutes from './routes/raffle.routes.js' // <- import nou
+...
+app.use('/api/raffles', raffleRoutes) // <- conectează rutele
